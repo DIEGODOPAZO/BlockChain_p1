f2dbea41a8ec153cde0c6c13b8893fd2e6d0eeb5
DIEGODOPAZO <diego.dopazo.gar@gmail.com>
2025-10-15 12:51:15 +0200

    feat: add functionalities related to users for buying and checking tickets

--- Ejercicio5/Lotery/contracts/LoteryUserFunctionalities.sol
+++ Ejercicio5/Lotery/contracts/LoteryUserFunctionalities.sol
@@ -1,1 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+
 // Funciones de usuarios
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./LotteryVariables.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract LotteryUserFunctionalities is LoteryVariables{
+    using SafeMath for uint256;
+
+    function buyTickets(uint256 lotteryId, uint256 quantity) external payable{
+        Lottery storage lot = lotteries[lotteryId];
+    
+        uint256 totalPrice = quantity.mul(lot.ticketPrice);
+
+        require(!lot.closed, "Lottery is closed");
+        require(quantity > 0, "Quantity must be greater than 0");
+        require(msg.value == totalPrice, "Invalid amount of ETH sent");
+
+        // Actualizar tickets vendidos
+        lot.ticketsSold = lot.ticketsSold.add(quantity);
+
+        // Actualizar tickets del participante
+        lot.ticketsByAddress[msg.sender] = lot.ticketsByAddress[msg.sender].add(quantity);
+
+        // Agregar a participantes si es la primera vez
+        if (lot.ticketsByAddress[msg.sender] == quantity) {
+            lot.participants.push(msg.sender);
+        }
+
+        // Actualizar el pot
+        lot.pot = lot.pot.add(msg.value);
+
+        // Emitir evento
+        emit TicketPurchased(lotteryId, msg.sender, quantity);
+
+    }
+
+
+    function getMyTickets(uint256 lotteryId, address participant) external view returns (uint256){
+        return lotteries[lotteryId].ticketsByAddress[participant];
+    }
+}
