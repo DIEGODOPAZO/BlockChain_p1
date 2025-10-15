324ee1a193bd4dc1b4a2a609d1dadab1f186cf36
DIEGODOPAZO <diego.dopazo.gar@gmail.com>
2025-10-15 11:28:13 +0200

    feat: add basic files to lotery

---
+++ Ejercicio5/Lotery/contracts/GetRandom.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract GetRandom {
+
+    // This contract simulates the genration of a random file
+
+    function getRandom() public pure returns(uint){
+        return 1;
+    }
+}
